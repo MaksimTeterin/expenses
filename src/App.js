57d6cb597d6397448bdbbe4e72f93c83e78db6ad
maksimteterin <maksim.padriku@gmail.com>
2024-04-10 09:52:12 +0300

    +data for components

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import "./App.css";
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      date: new Date(2024, 10, 4),
+      title: "New book",
+      price: 10,
+    },
+    {
+      date: new Date(2024, 19, 8),
+      title: "New jeans",
+      price: 100,
+    },
+  ];
   return (
     <div className="App">
-      <ExpenseItem />
-      <ExpenseItem />
+      <ExpenseItem expenseData={expenses[0]} />
+      <ExpenseItem expenseData={expenses[1]} />
     </div>
   );
 }
